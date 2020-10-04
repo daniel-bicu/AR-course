401af4cc57d2352904cdd29aae7d45b0a6a661f9
daniel-bicu <danielyoi1233@gmail.com>
2020-10-04 20:45:23 +0300

    HW1 added

---
+++ Assets/animatorStateController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class animatorStateController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Animator animator;
+    public GameObject camera;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+            UnityEngine.Debug.Log(Vector3.Distance(camera.transform.position, this.transform.position));
+            animator.SetBool("isWalking", false);
+            if(Vector3.Distance(camera.transform.position, this.transform.position) <= 0.3f)
+            {
+                animator.SetBool("isWalking", true);
+            }
+    }
+}
